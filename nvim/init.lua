03128f5ef7fc7d7a57917a026057cd237be8d4df
BayonetArch <sudipxd124@gmail.com>
2025-07-20 16:49:02 +0545

    added some plugins and also changed configs
    # plugins added #
       - smear-nvim [for cool cursor animations]
    # config changed #
      - in init.lua changed the colorscheme and its background

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require("config.lazy")
 require("config.keybinds")
 require("config.options")
-vim.cmd("colorscheme wildcharm ")
+vim.cmd("colorscheme habamax")
+vim.api.nvim_set_hl(0, 'Normal', { bg = '#0a0a0a' })
 
